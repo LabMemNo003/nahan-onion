731dd4f5c251033baec72af282c86bca70130d07
LabMemNo003 <LabMemNo003@gmail.com>
2020-08-08 02:57:54 +0800

    Implement Circuit()

--- test/circuit.js
+++ test/circuit.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Branch, Pipeline } = require('..');
+const { Circuit, Pipeline } = require('..');
 const expect = require('chai').expect;
 const _ = require('lodash');
 
@@ -10,18 +10,17 @@ const {
     EndWare,
     RetWare,
     MidWareTrue,
-    MidWareTrueFalse,
     EndWareTrue,
     MidWareFalse,
 } = require('./util');
 
-describe('Branch', () => {
+describe('Circuit', () => {
 
     describe('Basic usage', () => {
 
         it('Condition is true (1)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 EndWareTrue(arr, [2, 3], [4, 5], 0, 1)
             )(
                 ctx,
@@ -35,21 +34,21 @@ describe('Branch', () => {
 
         it('Condition is true (2)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
-                EndWareTrue(arr, [2, 3], [4, 5], 0, 1)
+            const rets = await Circuit(
+                MidWareTrue(arr, [2, 3], [8, 9], 0, 3)
             )(
                 ctx,
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                EndWare(arr, [4, 5], [6, 7], 1, 2),
                 0, 1
             );
             for (const r of rets) await asyncArrayPush(arr, r);
-            expect(arr).to.eql(_.range(6));
-            expect(ctx).to.eql(_.range(2));
+            expect(arr).to.eql(_.range(10));
+            expect(ctx).to.eql(_.range(4));
         });
 
         it('Condition is true (3)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 MidWareTrue(arr, [2, 3], [8, 9], 0, 3),
                 EndWare(arr, [4, 5], [6, 7], 1, 2)
             )(
@@ -64,26 +63,26 @@ describe('Branch', () => {
 
         it('Condition is true (4)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
-                MidWareTrue(arr, [2, 3], [8, 9], 0, 3),
-                EndWare(arr, [4, 5], [6, 7], 1, 2)
+            const rets = await Circuit(
+                MidWareTrue(arr, [2, 3], [12, 13], 0, 5),
+                MidWare(arr, [4, 5], [10, 11], 1, 4)
             )(
                 ctx,
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                EndWare(arr, [6, 7], [8, 9], 2, 3),
                 0, 1
             );
             for (const r of rets) await asyncArrayPush(arr, r);
-            expect(arr).to.eql(_.range(10));
-            expect(ctx).to.eql(_.range(4));
+            expect(arr).to.eql(_.range(14));
+            expect(ctx).to.eql(_.range(6));
         });
 
         it('Condition is false (1)', async () => {
-            await Branch()();
+            await Circuit()();
         });
 
         it('Condition is false (2)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 MidWareFalse(arr, 0)
             )(
                 ctx,
@@ -97,7 +96,7 @@ describe('Branch', () => {
 
         it('Condition is false (3)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 MidWareFalse(arr, 0)
             )(
                 ctx,
@@ -111,7 +110,7 @@ describe('Branch', () => {
 
         it('Condition is false (4)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 undefined,
                 MidWare(arr, [-1, -1], [-1, -1], -1, -1)
             )(
@@ -126,7 +125,7 @@ describe('Branch', () => {
 
         it('Condition is false (5)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 undefined,
                 MidWare(arr, [-1, -1], [-1, -1], -1, -1)
             )(
@@ -141,7 +140,7 @@ describe('Branch', () => {
 
         it('Condition is false (6)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 MidWareFalse(arr, 0),
                 MidWare(arr, [-1, -1], [-1, -1], -1, -1)
             )(
@@ -157,17 +156,34 @@ describe('Branch', () => {
 
     describe('Nested usage', () => {
 
-        it('Condition is B(true,B(true,1))(-1)', async () => {
+        it('Condition is C(true,C(true,1))(2)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
-                MidWareTrue(arr, [2, 3], [12, 13], 0, 5),
-                Branch(
-                    MidWareTrue(arr, [4, 5], [10, 11], 1, 4),
-                    EndWare(arr, [6, 7], [8, 9], 2, 3)
+            const rets = await Circuit(
+                MidWareTrue(arr, [2, 3], [16, 17], 0, 7),
+                Circuit(
+                    MidWareTrue(arr, [4, 5], [14, 15], 1, 6),
+                    MidWare(arr, [6, 7], [12, 13], 2, 5)
                 )
             )(
                 ctx,
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                EndWare(arr, [8, 9], [10, 11], 3, 4),
+                0, 1
+            );
+            for (const r of rets) await asyncArrayPush(arr, r);
+            expect(arr).to.eql(_.range(18));
+            expect(ctx).to.eql(_.range(8));
+        });
+
+        it('Condition is C(true,C(true,undefined))(1)', async () => {
+            const arr = [], ctx = [];
+            const rets = await Circuit(
+                MidWareTrue(arr, [2, 3], [12, 13], 0, 5),
+                Circuit(
+                    MidWareTrue(arr, [4, 5], [10, 11], 1, 4)
+                )
+            )(
+                ctx,
+                EndWare(arr, [6, 7], [8, 9], 2, 3),
                 0, 1
             );
             for (const r of rets) await asyncArrayPush(arr, r);
@@ -175,32 +191,32 @@ describe('Branch', () => {
             expect(ctx).to.eql(_.range(6));
         });
 
-        it('Condition is B(true,B(false,-1))(-1)', async () => {
+        it('Condition is C(true,C(false,-1))(1)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
-                MidWareTrueFalse(arr, [2, 3], [4, 5], [6, 7], 0, 2),
-                Branch(
+            const rets = await Circuit(
+                MidWareTrue(arr, [2, 3], [8, 9], 0, 4),
+                Circuit(
                     MidWareFalse(arr, 1),
                     MidWare(arr, [-1, -1], [-1, -1], -1, -1)
                 )
             )(
                 ctx,
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                EndWare(arr, [4, 5], [6, 7], 2, 3),
                 0, 1
             );
             for (const r of rets) await asyncArrayPush(arr, r);
-            expect(arr).to.eql(_.range(8));
-            expect(ctx).to.eql(_.range(3));
+            expect(arr).to.eql(_.concat(_.range(4), _.range(2, 10)));
+            expect(ctx).to.eql(_.range(5));
         });
 
-        it('Condition is B(false,-1)(B(true,1))', async () => {
+        it('Condition is C(false,-1)(C(true,1))', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 MidWareFalse(arr, 0),
                 MidWare(arr, [-1, -1], [-1, -1], -1, -1)
             )(
                 ctx,
-                Branch(
+                Circuit(
                     MidWareTrue(arr, [2, 3], [8, 9], 1, 4),
                     EndWare(arr, [4, 5], [6, 7], 2, 3)
                 ),
@@ -211,14 +227,31 @@ describe('Branch', () => {
             expect(ctx).to.eql(_.range(5));
         });
 
-        it('Condition is B(false,-1)(B(false,-1))', async () => {
+        it('Condition is C(false,-1)(C(true,undefined))', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
+            const rets = await Circuit(
                 MidWareFalse(arr, 0),
                 MidWare(arr, [-1, -1], [-1, -1], -1, -1)
             )(
                 ctx,
-                Branch(
+                Circuit(
+                    EndWareTrue(arr, [2, 3], [4, 5], 1, 2)
+                ),
+                0, 1
+            );
+            for (const r of rets) await asyncArrayPush(arr, r);
+            expect(arr).to.eql(_.concat(_.range(2), _.range(6)));
+            expect(ctx).to.eql(_.range(3));
+        });
+
+        it('Condition is C(false,-1)(C(false,-1))', async () => {
+            const arr = [], ctx = [];
+            const rets = await Circuit(
+                MidWareFalse(arr, 0),
+                MidWare(arr, [-1, -1], [-1, -1], -1, -1)
+            )(
+                ctx,
+                Circuit(
                     MidWareFalse(arr, 1),
                     MidWare(arr, [-1, -1], [-1, -1], -1, -1)
                 ),
@@ -229,40 +262,40 @@ describe('Branch', () => {
             expect(ctx).to.eql(_.range(2));
         });
 
-        it('Condition is B(B(true,1),-1)(2)', async () => {
+        it('Condition is C(C(true,1),2)(3)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
-                Branch(
-                    MidWareTrue(arr, [2, 3], [-1, -1], 0, 3),
-                    EndWare(arr, [4, 5], [6, 7], 1, 2)
+            const rets = await Circuit(
+                Circuit(
+                    MidWareTrue(arr, [2, 3], [16, 17], 0, 7),
+                    MidWare(arr, [4, 5], [14, 15], 1, 6)
                 ),
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1)
+                MidWare(arr, [6, 7], [12, 13], 2, 5)
             )(
                 ctx,
-                EndWare(arr, [10, 11], [12, 13], 4, 5),
+                EndWare(arr, [8, 9], [10, 11], 3, 4),
                 0, 1
             );
             for (const r of rets) await asyncArrayPush(arr, r);
-            expect(arr).to.eql(_.concat(_.range(8), _.range(2), _.range(10, 14)));
-            expect(ctx).to.eql(_.range(6));
+            expect(arr).to.eql(_.range(18));
+            expect(ctx).to.eql(_.range(8));
         });
 
-        it('Condition is B(B(false,-1),1)(-1)', async () => {
+        it('Condition is C(C(false,-1),1)(2)', async () => {
             const arr = [], ctx = [];
-            const rets = await Branch(
-                Branch(
+            const rets = await Circuit(
+                Circuit(
                     MidWareFalse(arr, 0),
                     MidWare(arr, [-1, -1], [-1, -1], -1, -1)
                 ),
-                EndWare(arr, [2, 3], [4, 5], 1, 2)
+                MidWare(arr, [2, 3], [8, 9], 1, 4)
             )(
                 ctx,
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                EndWare(arr, [4, 5], [6, 7], 2, 3),
                 0, 1
             );
             for (const r of rets) await asyncArrayPush(arr, r);
-            expect(arr).to.eql(_.concat(_.range(2), _.range(6)));
-            expect(ctx).to.eql(_.range(3));
+            expect(arr).to.eql(_.concat(_.range(2), _.range(10)));
+            expect(ctx).to.eql(_.range(5));
         });
     });
 
@@ -271,33 +304,33 @@ describe('Branch', () => {
         it('Condition is true', async () => {
             const arr = [], ctx = [];
             const rets = await Pipeline(
-                MidWare(arr, [2, 3], [20, 21], 0, 9),
-                Branch(
+                MidWare(arr, [2, 3], [28, 29], 0, 13),
+                Circuit(
                     Pipeline(
-                        MidWare(arr, [4, 5], [18, 19], 1, 8),
-                        MidWare(arr, [6, 7], [16, 17], 2, 7),
+                        MidWare(arr, [4, 5], [26, 27], 1, 12),
+                        MidWare(arr, [6, 7], [24, 25], 2, 11),
                     ),
                     Pipeline(
-                        MidWare(arr, [8, 9], [14, 15], 3, 6),
-                        EndWare(arr, [10, 11], [12, 13], 4, 5),
+                        MidWare(arr, [8, 9], [22, 23], 3, 10),
+                        MidWare(arr, [10, 11], [20, 21], 4, 9),
                     )
                 ),
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                MidWare(arr, [12, 13], [18, 19], 5, 8),
             )(
                 ctx,
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                EndWare(arr, [14, 15], [16, 17], 6, 7),
                 0, 1
             );
             for (const r of rets) await asyncArrayPush(arr, r);
-            expect(arr).to.eql(_.range(22));
-            expect(ctx).to.eql(_.range(10));
+            expect(arr).to.eql(_.range(30));
+            expect(ctx).to.eql(_.range(14));
         });
 
         it('Condition is false', async () => {
             const arr = [], ctx = [];
             const rets = await Pipeline(
                 MidWare(arr, [2, 3], [24, 25], 0, 12),
-                Branch(
+                Circuit(
                     Pipeline(
                         MidWare(arr, [4, 5], [-1, -1], 1, 5),
                         MidWare(arr, [6, 7], [10, 11], 2, 4),
@@ -320,11 +353,11 @@ describe('Branch', () => {
             expect(ctx).to.eql(_.range(13));
         });
 
-        it('Condition is P(B(false,-1),B(true,1))', async () => {
+        it('Condition is P(C(false,-1),C(true,1))', async () => {
             const arr = [], ctx = [];
             const rets = await Pipeline(
-                MidWare(arr, [2, 3], [28, 29], 0, 14),
-                Branch(
+                MidWare(arr, [2, 3], [36, 37], 0, 18),
+                Circuit(
                     Pipeline(
                         MidWare(arr, [4, 5], [-1, -1], 1, 5),
                         MidWare(arr, [6, 7], [10, 11], 2, 4),
@@ -333,25 +366,25 @@ describe('Branch', () => {
                     ),
                     MidWare(arr, [-1, -1], [-1, -1], -1, -1),
                 ),
-                Branch(
+                Circuit(
                     Pipeline(
-                        MidWare(arr, [12, 13], [26, 27], 6, 13),
-                        MidWare(arr, [14, 15], [24, 25], 7, 12),
+                        MidWare(arr, [12, 13], [34, 35], 6, 17),
+                        MidWare(arr, [14, 15], [32, 33], 7, 16),
                     ),
                     Pipeline(
-                        MidWare(arr, [16, 17], [22, 23], 8, 11),
-                        EndWare(arr, [18, 19], [20, 21], 9, 10),
+                        MidWare(arr, [16, 17], [30, 31], 8, 15),
+                        MidWare(arr, [18, 19], [28, 29], 9, 14),
                     )
                 ),
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                MidWare(arr, [20, 21], [26, 27], 10, 13),
             )(
                 ctx,
-                MidWare(arr, [-1, -1], [-1, -1], -1, -1),
+                EndWare(arr, [22, 23], [24, 25], 11, 12),
                 0, 1
             );
             for (const r of rets) await asyncArrayPush(arr, r);
-            expect(arr).to.eql(_.concat(_.range(12), _.range(2, 4), _.range(12, 30)));
-            expect(ctx).to.eql(_.range(15));
+            expect(arr).to.eql(_.concat(_.range(12), _.range(2, 4), _.range(12, 38)));
+            expect(ctx).to.eql(_.range(19));
         });
     });
 });
